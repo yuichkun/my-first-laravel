05435092ddf8980710d049d459b06d155a3dda3b
Yuichi Yogo <yuichkun@gmail.com>
2019-08-17 19:45:53 +0900

    add factory for posts

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,9 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('password'),
         ]);
         
-        factory(App\User::class, 50)->create();
+        // factory(App\User::class, 50)->create();
+        factory(App\User::class, 50)->create()->each(function ($user){
+            $user->posts()->save(factory(App\Post::class)->make());
+        });
     }
 }
